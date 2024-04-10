33120a9fb572f10f3882477c828f73ffab03663e
dselmer <dselmer@icstars.org>
2024-04-10 08:24:00 -0500

    first commit

---
+++ js/TodoFunctions/todoFunctions.js
@@ -0,0 +1,49 @@
+
+
+export const addTodos = (todo) => {
+    const arr = [];
+    if (!todo) return null
+    if (typeof todo !== 'string') {
+        return null
+    }
+    if (todo) {
+        arr.push(todo);
+    }
+    console.log(arr)
+    return arr
+};
+
+// export const addTwo = (num) => {
+//     if (typeof num !== 'number') {
+//         return new Error('must be a number');
+//     }
+//     return num + 2;
+
+// }
+
+
+
+
+
+
+export const getTodos = (arr) => {
+    if (arr.length === 0) return null;
+    return arr
+}
+
+const deleteTodos = (todoList, todo) => {
+    if (todoList.includes(todo)) {
+
+        let todoItem = todoList.IndexOf(todo);
+        const newTodoList = todoList.toSplice(todoItem, 1)
+        originalTodoList.splice(0, originalTodoList.length)
+        newTodoList.map((todoItem) => {
+            originalTodoList.push(todoItem)
+            return
+        })
+
+
+    }
+
+}
+
---
+++ js/TodoFunctions/todoFunctions.test.js
@@ -0,0 +1,43 @@
+import {addTodos,getTodos,addTwo} from './todoFunctions'
+// const {getTodos} = require('./todoFunctions');
+
+// describe("addTwo function", () => {
+ 
+//     test("checks if todo is a string", () => {
+//          expect(() => {  addTwo('s'); }).toThrow('must be a number');
+    
+      
+//     });
+
+// })
+
+
+describe("addtodos function", () => {
+    const expected = ['todo']
+    test("checks todo list for added todo", () => {
+        expect(addTodos('todo')).toStrictEqual(expected);
+    });
+    test("checks todo item was undefined ", () => {
+        expect(addTodos('')).toBeFalsy();
+    });
+    test("checks if todo is a string", () => {
+         expect(addTodos(3)).toBeFalsy();
+    
+    });
+
+})
+
+describe("gettodos function", () => {
+    const expected = ['todo1', 'todo2', 'todo3']
+    const emptyTodoList = [];
+    test("checks todo list for added todo", () => {
+        expect(getTodos(expected)).toEqual(expected);
+        expect(getTodos(expected)).toHaveLength(3);
+
+    });
+    test("returns empty todoList", () => {
+        expect(getTodos(emptyTodoList)).toBeFalsy();
+
+    });
+
+})
