37042dd79c103eead8b624e7cce866e77d2cc527
dselmer <dselmer@icstars.org>
2024-04-12 19:01:17 -0500

    fixed add todo list feature

--- js/main.js
+++ js/main.js
@@ -1,17 +1,8 @@
 
 
 const container = document.querySelector('.container');
-
-
-// task.innerText='build app in js'
-
-console.log('coming from main');
-
 const input = document.querySelector('.inputs');
 const submitBtn = document.querySelector('.submit');
-
-
-
 
 export const addTodos = (todo) => {
     const arr = [];
@@ -27,72 +18,32 @@ export const addTodos = (todo) => {
 };
 
 let inputValue;
-const todoList  = [];
+const todoList = [];
 
-console.log(input);
+function submitHandler(node) {
+    for (const todo of node) {
 
-function submitHandler(node){
-
-
-
-for (const todo of node) {
-   
-    const taskContiner = document.createElement("div");
-    container.appendChild(taskContiner);
-    taskContiner.className ='task-container';
-    const task =  document.createElement("h3");
-    taskContiner.appendChild(task);
-    task.className='task';
-    task.innerText = todo;
- 
-  }
-
-
+        const taskContiner = document.createElement("div");
+        container.appendChild(taskContiner);
+        taskContiner.className = 'task-container';
+        const task = document.createElement("h3");
+        taskContiner.appendChild(task);
+        task.className = 'task';
+        task.innerText = todo;
+    }
 }
 
-
-input.addEventListener("change", function(e){
+input.addEventListener("change", function (e) {
     inputValue = e.target.value;
     e.target.value = '';
+});
 
-
-  });
-
-  submitBtn.addEventListener("click", function(){
-    if(!inputValue)return null;
-    todoList.push(inputValue)
-    inputValue= '';
-    submitHandler(todoList);
-    });
-
-
-  
-
-
-
-
-
-
-export const getTodos = (arr) => {
-    if (arr.length === 0) return null;
-    return arr
-}
-
-const deleteTodos = (todoList, todo) => {
-    if (todoList.includes(todo)) {
-
-        let todoItemIndex = todoList.IndexOf(todo);
-        const newTodoList = todoList.toSplice(todoItemIndex, 1)
-        originalTodoList.splice(0, originalTodoList.length)
-        newTodoList.map((todoItem) => {
-            originalTodoList.push(todoItem)
-            return
-        })
-
-
+submitBtn.addEventListener("click", function () {
+    if (!inputValue) return null;
+    if (!todoList.includes(inputValue)) {
+        todoList.push(inputValue)
+        submitHandler(todoList);
+        inputValue = '';
+        return todoList.length = 0;
     }
-
-}
-
-
-
+});
